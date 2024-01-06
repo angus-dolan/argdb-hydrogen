08bd86ba93615efa369e1dc04a26bfdecaa91129
Angus <angusdolan100@btinternet.com>
2024-01-06 16:06:08 +0000

    WIP: Strategy pattern boilerplate for lexer

--- src/sadface_translator/lexer.py
+++ src/sadface_translator/lexer.py
@@ -1,6 +1,25 @@
-class Lexer:
-  def __init__(self, input_data):
-    self.input = input_data
+from abc import ABC, abstractmethod
+import json
 
-  def tokenize(self):
-    print(self.input)
+class LexerStrategy(ABC):
+  @abstractmethod
+  def tokenize(self, data):
+    pass
+
+class ArgsmeLexer(LexerStrategy):
+  def tokenize(self, data):
+    try:
+      # Tokenizing JSON data
+      return data
+    except json.JSONDecodeError:
+      return "Invalid JSON"
+
+class LexerContext:
+  def __init__(self, lexer_strategy: LexerStrategy):
+    self._lexer_strategy = lexer_strategy
+
+  def set_lexer_strategy(self, lexer_strategy: LexerStrategy):
+    self._lexer_strategy = lexer_strategy
+
+  def tokenize_data(self, data):
+    return self._lexer_strategy.tokenize(data)
