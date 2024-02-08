f31b76f302c1701b8a40559a06a19676103788fb
Angus <angusdolan100@btinternet.com>
2024-02-08 14:38:04 +0000

    WIP: Deciding states for moore machine parser

--- hydrogen/importer/parser.py
+++ hydrogen/importer/parser.py
@@ -1,6 +1,7 @@
 from .models.tokens import Token, ArgsmeToken
 from .builder import SadfaceBuilder
 from abc import ABC, abstractmethod
+import sys
 import uuid
 import hashlib
 import json
@@ -12,6 +13,13 @@ class BaseParser(ABC):
 
     def get_parsed_sf_doc(self):
         return self._parsed_sf_doc
+
+    def _string_to_uuid(string):
+        if string == "":
+            return ""
+        hashed = hashlib.sha1(string.encode()).hexdigest()
+        shortened = hashed[:32]
+        return str(uuid.UUID(shortened))
 
     @abstractmethod
     def parse(self):
@@ -36,6 +44,34 @@ class ArgsmeParser(BaseParser):
     def __init__(self, lexed_tokens: list):
         super().__init__()
         self._lexed_tokens = lexed_tokens
+        self._current_state = 'start'
+        # TODO: Return lexed_tokens from lexer as a hashmap with ArgsmeToken as key, for O(1)
+        # NOTE: Maybe the state transitions should be 'build_new_doc', 'update_existing_doc' etc.
+        # NOTE: Think about mem management, if I've got 1GB in mem, I could end up needing an another GB unless I free the memory as we parse.
+        self.DYNAMIC_TRANSITIONS = {
+            'update_or_new': self.decide_update_or_new,
+            'another_state': self.decide_for_another_state
+        }
+        self.STATE_TRANSITIONS = {
+            'start': 'build_node',
+            'build_node': 'update_or_new',
+            'update': None,
+            'new': None,
+        }
+        # self.STATE_TRANSITIONS = {
+        #     'start': 'premises_stance',
+        #     'premises_stance': 'premises_text',
+        #     'premises_text': 'ctx_acq_time',
+        #     'ctx_acq_time': 'ctx_src_id',
+        #     'ctx_src_id': 'ctx_prev_id',
+        #     'ctx_prev_id': 'ctx_title',
+        #     'ctx_title': 'id',
+        #     'id': 'conclusion',
+        #     'conclusion': 'end'
+        # }
+        self.STATE_HANDLERS = {
+            ArgsmeToken.PREMISES_STANCE: 'premises_stance',
+        }
 
     def parse(self):
         pass
