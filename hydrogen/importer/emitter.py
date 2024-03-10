3f7c02df7a782c0bbfdf5eb8e34919547233ea47
Angus <angusdolan100@btinternet.com>
2024-03-10 16:46:00 +0000

    WIP: Investigated better edge ngrams generation and a lookup table approach

--- hydrogen/importer/emitter.py
+++ hydrogen/importer/emitter.py
@@ -16,14 +16,14 @@ class Emitter:
         self.document = doc
 
     def emit(self):
-        exists = db.arguments.get(uuid=self.uuid)
-        db_payload = json.dumps(self.document)
-
-        if exists:
-            db.arguments.update(uuid=self.uuid, payload=db_payload)
-            # TODO: Update an existing search index
-            return
-
-        new_record = ArgumentModel(uuid=self.uuid, data=db_payload)
-        db.arguments.add(payload=new_record)
+        # exists = db.arguments.get(uuid=self.uuid)
+        # db_payload = json.dumps(self.document)
+        #
+        # if exists:
+        #     db.arguments.update(uuid=self.uuid, payload=db_payload)
+        #     # TODO: Update an existing search index
+        #     return
+        #
+        # new_record = ArgumentModel(uuid=self.uuid, data=db_payload)
+        # db.arguments.add(payload=new_record)
         search_engine.add_document(self.uuid, self.document)
--- hydrogen/search/engine.py
+++ hydrogen/search/engine.py
@@ -2,26 +2,36 @@ from hydrogen.search.parser import SearchParser
 from hydrogen.search.index import SearchIndex
 from collections import Counter
 from pyinstrument import Profiler
+from nltk.util import ngrams
+
 
 class SearchEngine:
     def __init__(self):
         self.search_parser = SearchParser()
         self.index = SearchIndex()
 
-    def generate_concordance(self, parsed_text):
-        concordance = Counter()
-        for token in parsed_text:
-            edge_ngrams = self.index.generate_edge_ngrams(token)
-            concordance.update(edge_ngrams)
-        return concordance
+    # def generate_concordance(self, parsed_text):
+    #     concordance = Counter()
+    #     for token in parsed_text:
+    #         edge_ngrams = self.index.generate_edge_ngrams(token)
+    #         concordance.update(edge_ngrams)
+    #     return concordance
+
+    def generate_edge_ngrams(self, text, max_length=3):
+        edge_ngrams = []
+        for word in text.split():
+            for end in range(1, min(len(word), max_length) + 1):
+                edge_ngrams.append(word[:end])
+        return edge_ngrams
 
     def add_document(self, doc_id, sadface_doc):
         nodes = sadface_doc.get('nodes')
 
         for node in nodes:
             parsed_text = self.search_parser.parse(node.get('text'))
-            concordance = self.generate_concordance(parsed_text)
-            self.index.add_to_index(doc_id, concordance)
+            edge_ngrams = self.generate_edge_ngrams(' '.join(parsed_text))
+            n = self.generate_edge_ngrams("mens mental health")
+            print()
 
     def search(self, query):
         parsed_query = self.search_parser.parse(query)
@@ -31,15 +41,16 @@ class SearchEngine:
 
 
 def main():
+    var = {
+        "nodes": [
+            {"id": "665a55a3-2019-04-19T12:46:15Z-00004-000", "metadata": {"stance": "PRO"},
+             "text": "Physically fit people have better mental health and do better at academia", "type": "atom"},
+        ]
+    }
+
     engine = SearchEngine()
-    # query = input("Enter Search Term: ")
-    query = "Something about high schools"
-    results = engine.search(query)
+    engine.add_document("665a55a3-2019-04-19T12:46:15Z", var)
 
-    for relation, doc in results:
-        print(f"Relevance: {relation}, Document: {doc[:200]}...")
-
-    print("Number of results:" + str(len(results)))
 
 def profile_search_engine():
     profiler = Profiler()
@@ -49,6 +60,7 @@ def profile_search_engine():
 
     profiler.stop()
     print(profiler.output_text(unicode=True, color=True))
+
 
 if __name__ == "__main__":
     profile_search_engine()
